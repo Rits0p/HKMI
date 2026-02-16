af8367aae85ebae5d0f8bf2900a4ce1175c67937
Ritesh Patel <patelriteshd717@gmail.com>
2026-02-16 12:27:10 +0530

    created model for event page

--- hkm/hkm/settings.py
+++ hkm/hkm/settings.py
@@ -81,8 +81,12 @@ WSGI_APPLICATION = 'hkm.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'hkm_web',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
